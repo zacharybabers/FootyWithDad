59a59fc220bd00d938d7b1fe8f02d0a166c4c561
Zachary Babers <zacharybabers@gmail.com>
2022-08-13 22:04:58 -0400

    Initial commit

---
+++ Assets/Scripts/Ball/BallShadow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallShadow : MonoBehaviour
+{
+    [SerializeField] private Transform ballTransform;
+    private float initBallHeight;
+    private Vector3 initScale;
+
+    void Start()
+    {
+        initBallHeight = ballTransform.position.y;
+        initScale = transform.localScale;
+    }
+    void Update()
+    {
+        TrackBallXZ();
+        UpdateScale();
+    }
+
+    private void TrackBallXZ()
+    {
+        var height = transform.position.y;
+        var ballPosition = ballTransform.position;
+        var newPosition = new Vector3(ballPosition.x, height, ballPosition.z);
+        transform.position = newPosition;
+    }
+
+    private void UpdateScale()
+    {
+        var normalizedHeight = ballTransform.position.y - initBallHeight;
+        var newMult = ((2f / (1f + Mathf.Exp(normalizedHeight / 5f))) + 1f) / 2f;
+        newMult *= initScale.z;
+        transform.localScale = new Vector3(newMult, newMult, newMult);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private float movementSpeed = 10f;
+    private CharacterController characterController;
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(GetLookPosition());
+        DoMovement();
+    }
+
+    private void DoMovement()
+    {
+        var inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        var camTransform = mainCamera.transform;
+        var flatForward = camTransform.forward;
+        flatForward.y = 0;
+        var flatRight = camTransform.right;
+        flatRight.y = 0;
+        var movementVector = flatRight.normalized + flatForward.normalized;
+        movementVector *= movementSpeed * Time.deltaTime;
+        movementVector.x *= inputVector.x;
+        movementVector.z *= inputVector.y;
+        characterController.Move(movementVector);
+    }
+
+    private Vector3 GetLookPosition()
+    {
+        var result = Vector3.zero;
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            result = hit.point;
+        }
+
+        result.y = transform.position.y;
+        return result;
+    }
+}
